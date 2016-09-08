4900616ec2777e6fe7fba8993b46c9d246a4b6e4
Ravi K <ravikiran438@gmail.com>
2016-09-08 18:02:48 -0400

    add some plugins

--- server.js
+++ server.js
@@ -3,9 +3,28 @@ var prerender = require('./lib');
 
 var server = prerender({
     workers: process.env.PRERENDER_NUM_WORKERS,
-    iterations: process.env.PRERENDER_NUM_ITERATIONS
+    iterations: process.env.PRERENDER_NUM_ITERATIONS,
+    logRequests: true,
+    jsTimeout: 30000,
+    accessLog: {
+        // Check out the file-stream-rotator docs for parameters
+        fileStreamRotator: {
+            filename: './logs/access-%DATE%.log',
+            frequency: 'daily',
+            date_format: 'YYYY-MM-DD',
+            verbose: false
+        },
+
+        // Check out the morgan docs for the available formats
+        morgan: {
+            format: 'combined'
+        }
+    }
 });
 
+server.use(prerender.levelCache());
+server.use(prerender.monitoring());
+server.use(require('prerender-access-log'));
 
 server.use(prerender.sendPrerenderHeader());
 // server.use(prerender.basicAuth());
